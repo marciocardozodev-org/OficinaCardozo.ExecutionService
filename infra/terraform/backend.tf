f4e4c0afffae10a655c27f9f3155d3ba3ae89ce4
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-02-16 23:52:36 +0000

    feat: ajusta chave do backend do Terraform para billingservice-terraform.tfstate

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "oficina-cardozo-terraform-state-sp"
-    key     = "db/prod/terraform.tfstate"
+    key     = "eks/prod/billingservice-terraform.tfstate"
     region  = "sa-east-1"
     encrypt = true
   }
